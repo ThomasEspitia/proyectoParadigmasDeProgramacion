2b3b4f6a833c8c717203d3b29eb9d2ecacffaf5d
Riiperk <riiperk47@gmail.com>
2025-04-27 22:22:51 -0500

    Update CRUD.java

--- Model/co.edu.poli.proyecto2.servicios/CRUD.java
+++ Model/co.edu.poli.proyecto2.servicios/CRUD.java
@@ -5,38 +5,10 @@ import co.edu.poli.proyecto2.modelo.Semilla;
 import java.io.*;
 import java.util.*;
 
-/**
- * 
- */
 public interface CRUD {
-
-    /**
-     * @param semilla 
-     * @return
-     */
-    public String create(Semilla semilla);
-
-    /**
-     * @param id 
-     * @return
-     */
-    public Semilla read(String id);
-
-    /**
-     * @return
-     */
-    public List<Semilla> readAll();
-
-    /**
-     * @param id 
-     * @return
-     */
-    public String update(String id);
-
-    /**
-     * @param id 
-     * @return
-     */
-    public String delete(String id);
-
+    String create(Semilla semilla);
+    Semilla read(String id);
+    Semilla[] readAll();
+    String update(String id, Semilla semilla);
+    String delete(String id);
 }
