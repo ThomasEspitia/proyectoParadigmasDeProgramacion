b6e487a1ffd6526377363eb755bc5e9dedbdcb4c
Riiperk <riiperk47@gmail.com>
2025-04-27 21:53:25 -0500

    Update Hortaliza.java

--- Model/co.edu.poli.proyecto2.modelo/Hortaliza.java
+++ Model/co.edu.poli.proyecto2.modelo/Hortaliza.java
@@ -3,20 +3,20 @@ package co.edu.poli.proyecto2.modelo;
 import java.io.*;
 import java.util.*;
 
-/**
- * 
- */
-public class Hortaliza extends Semilla {
-
-    /**
-     * Default constructor
-     */
-    public Hortaliza() {
+public class Legumbre extends Semilla {
+    private double temperaturaIdeal;
+    public Legumbre(String id, String nombre, int cantidadDisponible, int tiempoDeCosecha, String origen, double temperaturaIdeal) {
+        super(id, nombre, cantidadDisponible, tiempoDeCosecha, origen);
+        this.temperaturaIdeal = temperaturaIdeal;
     }
-
-    /**
-     * 
-     */
-    private String tipoHortaliza;
-
+	public double getTemperaturaIdeal() {
+		return temperaturaIdeal;
+	}
+	public void setTemperaturaIdeal(double temperaturaIdeal) {
+		this.temperaturaIdeal = temperaturaIdeal;
+	}
+	@Override
+	public String toString() {
+		return super.toString() + ", Temperatura Ideal: " + temperaturaIdeal;		
+	}
 }
