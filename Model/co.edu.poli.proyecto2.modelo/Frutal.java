4f0dd6a147e30dfe2c4460d33412af561fbffe9c
Riiperk <riiperk47@gmail.com>
2025-04-27 21:47:27 -0500

    Update Frutal.java

--- Model/co.edu.poli.proyecto2.modelo/Frutal.java
+++ Model/co.edu.poli.proyecto2.modelo/Frutal.java
@@ -3,20 +3,24 @@ package co.edu.poli.proyecto2.modelo;
 import java.io.*;
 import java.util.*;
 
-/**
- * 
- */
-public class Frutal extends Semilla {
+public class Hortaliza extends Semilla {
+    private String tipoHortaliza;
 
-    /**
-     * Default constructor
-     */
-    public Frutal() {
+    public Hortaliza(String id, String nombre, int cantidadDisponible, int tiempoDeCosecha, String origen, String tipoHortaliza) {
+        super(id, nombre, cantidadDisponible, tiempoDeCosecha, origen);
+        this.tipoHortaliza = tipoHortaliza;
     }
 
-    /**
-     * 
-     */
-    private String mesFructificacion;
+	public String getTipoHortaliza() {
+		return tipoHortaliza;
+	}
 
+	public void setTipoHortaliza(String tipoHortaliza) {
+		this.tipoHortaliza = tipoHortaliza;
+	}
+
+	@Override
+	public String toString() {
+        return super.toString() + ", Tipo de Hortaliza: " + tipoHortaliza;
+    }
 }
