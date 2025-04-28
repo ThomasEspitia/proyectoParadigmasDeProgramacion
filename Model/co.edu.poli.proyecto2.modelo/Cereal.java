711c0a73a0447a1d64f073374132f5642109b272
Riiperk <riiperk47@gmail.com>
2025-04-27 22:00:46 -0500

    Update Cereal.java

--- Model/co.edu.poli.proyecto2.modelo/Cereal.java
+++ Model/co.edu.poli.proyecto2.modelo/Cereal.java
@@ -3,20 +3,25 @@ package co.edu.poli.proyecto2.modelo;
 import java.io.*;
 import java.util.*;
 
-/**
- * 
- */
 public class Cereal extends Semilla {
-
-    /**
-     * Default constructor
-     */
-    public Cereal() {
-    }
-
-    /**
-     * 
-     */
     private String tipoSuelo;
 
+    public Cereal(String id, String nombre, int cantidadDisponible, int tiempoDeCosecha, String origen, String tipoSuelo) {
+        super(id, nombre, cantidadDisponible, tiempoDeCosecha, origen);
+        this.tipoSuelo = tipoSuelo;
+    }
+
+	public String getTipoSuelo() {
+		return tipoSuelo;
+	}
+
+	public void setTipoSuelo(String tipoSuelo) {
+		this.tipoSuelo = tipoSuelo;
+	}
+
+    @Override
+    public String toString() {
+        return super.toString() + ", Tipo de Suelo: " + tipoSuelo;
+    }
+    
 }
