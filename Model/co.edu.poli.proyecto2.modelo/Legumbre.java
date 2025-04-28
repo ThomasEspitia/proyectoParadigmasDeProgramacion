ad2360a8a58589836e0dab3ff9e311381da9a5a1
Riiperk <riiperk47@gmail.com>
2025-04-27 21:55:51 -0500

    Update Legumbre.java

--- Model/co.edu.poli.proyecto2.modelo/Legumbre.java
+++ Model/co.edu.poli.proyecto2.modelo/Legumbre.java
@@ -3,20 +3,27 @@ package co.edu.poli.proyecto2.modelo;
 import java.io.*;
 import java.util.*;
 
-/**
- * 
- */
 public class Legumbre extends Semilla {
-
-    /**
-     * Default constructor
-     */
-    public Legumbre() {
-    }
-
-    /**
-     * 
-     */
     private double temperaturaIdeal;
 
+    public Legumbre(String id, String nombre, int cantidadDisponible, int tiempoDeCosecha, String origen, double temperaturaIdeal) {
+        super(id, nombre, cantidadDisponible, tiempoDeCosecha, origen);
+        this.temperaturaIdeal = temperaturaIdeal;
+    }
+
+	public double getTemperaturaIdeal() {
+		return temperaturaIdeal;
+	}
+
+	public void setTemperaturaIdeal(double temperaturaIdeal) {
+		this.temperaturaIdeal = temperaturaIdeal;
+	}
+
+	@Override
+	public String toString() {
+		return super.toString() + ", Temperatura Ideal: " + temperaturaIdeal;
+		
+	}
+    
+	
 }
