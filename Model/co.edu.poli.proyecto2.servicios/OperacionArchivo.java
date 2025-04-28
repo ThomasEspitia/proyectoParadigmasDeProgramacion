13544eb60789da74b4ec1ec61f429dd954d8842c
Riiperk <riiperk47@gmail.com>
2025-04-27 22:24:22 -0500

    Update OperacionArchivo.java

--- Model/co.edu.poli.proyecto2.servicios/OperacionArchivo.java
+++ Model/co.edu.poli.proyecto2.servicios/OperacionArchivo.java
@@ -3,21 +3,7 @@ package co.edu.poli.proyecto2.servicios;
 import java.io.*;
 import java.util.*;
 
-/**
- * 
- */
 public interface OperacionArchivo {
-
-    /**
-     * @param path 
-     * @return
-     */
-    public String serializar(String path);
-
-    /**
-     * @param path 
-     * @return
-     */
-    public String deserializar(String path);
-
+    String serializar(Semilla[] semillas, int total);
+    Semilla[] deserializar();
 }
